48b519bcc3b67c3ecceca5eac31adebfa5ad7761
Mawootad <adinfinatioma@gmail.com>
2023-12-28 10:08:45 -0800

    Make multi-headed hit for .25 on second if double headed and .2 then .15 if triple headed

--- src/battle_util.c
+++ src/battle_util.c
@@ -15437,15 +15437,15 @@ u32 CalcFinalDmg(u32 dmg, u16 move, u8 battlerAtk, u8 battlerDef, u8 moveType, u
 	if(abilityAtk == ABILITY_MULTI_HEADED || BattlerHasInnate(gBattlerAttacker, ABILITY_MULTI_HEADED)){
         if(gBaseStats[gBattleMons[gBattlerAttacker].species].flags & F_THREE_HEADED){
             if(gSpecialStatuses[gBattlerAttacker].parentalBondOn == 1){
-			MulModifier(&finalModifier, UQ_4_12(0.2));
+			MulModifier(&finalModifier, UQ_4_12(0.15));
             }
             else if(gSpecialStatuses[gBattlerAttacker].parentalBondOn == 2){
-                MulModifier(&finalModifier, UQ_4_12(0.15));
+                MulModifier(&finalModifier, UQ_4_12(0.2));
             }
         }
         else if(gBaseStats[gBattleMons[gBattlerAttacker].species].flags & F_TWO_HEADED){
             if(gSpecialStatuses[gBattlerAttacker].parentalBondOn == 1){
-			MulModifier(&finalModifier, UQ_4_12(0.2));
+			MulModifier(&finalModifier, UQ_4_12(0.25));
             }
         }
 	}
