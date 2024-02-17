54e8cb158364cf9a428aadcde3134b82f223ac71
Mawootad <adinfinatioma@gmail.com>
2024-02-17 11:39:42 -0800

    Smashin Realities supposed to be physical

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -10894,8 +10894,8 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 0,
         .target = MOVE_TARGET_SELECTED,
         .priority = -3,
-        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
-        .split = SPLIT_SPECIAL,
+        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGS_ROCK_AFFECTED,
+        .split = SPLIT_PHYSICAL
     },
     [MOVE_CREEPING_THORNS] =
     {
