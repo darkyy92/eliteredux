5ba80d05909b7a32e9b8c7814c6164cf6b371009
Mawootad <adinfinatioma@gmail.com>
2024-02-29 19:50:08 -0800

    Lunar Blessing

--- src/data/text/move_names.h
+++ src/data/text/move_names.h
@@ -916,6 +916,7 @@ const u8 gMoveNames[MOVES_COUNT][MOVE_NAME_LENGTH + 1] =
     [MOVE_PSYCHIC_NOISE] = _("PsychicNoise"),
     [MOVE_UPPER_HAND] = _("Upper Hand"),
     [MOVE_MALIGNANT_CHAIN] = _("Malig. Chain"),
+    [MOVE_LUNAR_BLESSING] = _("LunarBlessng"),
 };
 
 // Second table with longer move names for places where they fit.
@@ -1838,4 +1839,5 @@ const u8 gMoveNamesLong[MOVES_COUNT][LONG_MOVE_NAME_LENGTH + 1] =
     [MOVE_PSYCHIC_NOISE] = _("Psychic Noise"),
     [MOVE_UPPER_HAND] = _("Upper Hand"),
     [MOVE_MALIGNANT_CHAIN] = _("Malignant Chain"),
+    [MOVE_LUNAR_BLESSING] = _("Lunar Blessing"),
 };
