16250e8eefdb68fb8cfe3f1e5334f33f67c9c3c9
Mawootad <adinfinatioma@gmail.com>
2024-03-11 22:54:58 -0700

    Give Shelter to Hisuian Goodra

--- src/data/pokemon/level_up_learnsets.h
+++ src/data/pokemon/level_up_learnsets.h
@@ -26361,6 +26361,7 @@ static const struct LevelUpMove sBraviaryHisuianLevelUpLearnset[] = {
 
 static const struct LevelUpMove sSliggooHisuianLevelUpLearnset[] = {
     LEVEL_UP_MOVE(0, MOVE_ACID),
+    LEVEL_UP_MOVE(0, MOVE_SHELTER),
     LEVEL_UP_MOVE(1, MOVE_ABSORB),
     LEVEL_UP_MOVE(1, MOVE_BUBBLE),
     LEVEL_UP_MOVE(1, MOVE_TACKLE),
@@ -26382,7 +26383,8 @@ static const struct LevelUpMove sSliggooHisuianLevelUpLearnset[] = {
 };
 
 static const struct LevelUpMove sGoodraHisuianLevelUpLearnset[] = {
-    LEVEL_UP_MOVE(0, MOVE_ACID),
+    LEVEL_UP_MOVE(1, MOVE_ACID),
+    LEVEL_UP_MOVE(1, MOVE_SHELTER),
     LEVEL_UP_MOVE(1, MOVE_ABSORB),
     LEVEL_UP_MOVE(1, MOVE_BUBBLE),
     LEVEL_UP_MOVE(1, MOVE_TACKLE),
