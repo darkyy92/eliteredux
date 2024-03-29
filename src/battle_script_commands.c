ee666994aa5cfbe302725fcda9f316b4a538c68c
Mawootad <adinfinatioma@gmail.com>
2024-03-29 15:25:48 -0700

    Check item suppression for whether mon is holding iron ball

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2262,7 +2262,7 @@ s32 CalcCritChanceStage(u8 battlerAtk, u8 battlerDef, u32 move, bool32 recordAbi
              (( gBattleMons[battlerDef].status1 & STATUS1_PSN_ANY)                   ||
              (  gBattleMons[battlerDef].statStages[STAT_SPEED] < DEFAULT_STAT_STAGE) ||
              (  gBattleMons[battlerDef].status1 & STATUS1_PARALYSIS)                 ||
-             (  gBattleMons[battlerDef].item == ITEM_IRON_BALL)))
+             (  GetBattlerHoldEffect(battlerDef, TRUE) == HOLD_EFFECT_IRON_BALL)))
              || (BATTLER_HAS_ABILITY(battlerAtk, ABILITY_AMBUSH) && gVolatileStructs[battlerAtk].isFirstTurn)
              || (gVolatileStructs[battlerAtk].showdownMode)
              || (move == MOVE_SPACIAL_REND && BATTLER_HAS_ABILITY(battlerAtk, ABILITY_HEAVEN_ASUNDER)))
